9dec7547af58984e59f9c79edfcac7f10b1eaaeb
ritwikraha <rtzdzn@gmail.com>
2021-09-21 22:54:30 +0530

    final stretch of NERF manim

--- random-experiments/Manim-Mastery/3D-Camera/ThetaPhi.py
+++ random-experiments/Manim-Mastery/3D-Camera/ThetaPhi.py
@@ -10,26 +10,35 @@ class Visualizer(ThreeDScene):
                       worldAxes.get_x_axis_label(Tex("$X_w$").rotate(90 * DEGREES, axis=X_AXIS)),
                       worldAxes.get_y_axis_label(Tex("$Y_w$").rotate(90 * DEGREES, axis=X_AXIS))]
 
-        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES, distance=20)
+        self.set_camera_orientation(phi=75 * DEGREES, theta=0 * DEGREES, distance=20)
+
+        tracker = ValueTracker(0)
 
         phiText = MathTex(r"\phi=75").scale(0.8).to_corner(UL)
-        thetaText = MathTex(r"\theta=-45").scale(0.8).next_to(phiText, DOWN)
+        thetaText = MathTex(r"\theta=").scale(0.8).to_corner(UL).shift(0.5*DOWN)
 
-        updatedthetaText = MathTex(r"\theta=40").scale(0.8).next_to(phiText, DOWN)
+        numbers = DecimalNumber(0, num_decimal_places=0)
+        point = Dot3D([1.0,1.0,1.0], color=BLUE)
+
+        def numbers_updater(m):
+            m.set_value(tracker.get_value())
+            self.add_fixed_in_frame_mobjects(m)
+
+        numbers.add_updater(numbers_updater)
+        numbers.scale(0.8).next_to(thetaText)
 
         self.play(
             FadeIn(worldAxes, *labelWorld),
             FadeIn(self.add_fixed_in_frame_mobjects(phiText)),
-            FadeIn(self.add_fixed_in_frame_mobjects(thetaText))
+            FadeIn(self.add_fixed_in_frame_mobjects(thetaText)),
+            Create(point)
         )
 
         self.wait(2)
-
-        self.move_camera(
-            phi=75 * DEGREES,
-            theta=40 * DEGREES,
-            distance=20
+        self.play(
+            FadeIn(numbers)
         )
-        self.play(FadeOut(thetaText))
-        self.play(FadeIn(self.add_fixed_in_frame_mobjects(updatedthetaText)))
+
+        self.begin_ambient_camera_rotation(about="theta",rate=0.38)
+        self.play(tracker.animate.set_value(360), run_time=16)
         self.wait(1)
