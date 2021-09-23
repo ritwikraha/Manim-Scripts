5856bfdaabd3f0118a6c0d5652c48b46f37baa09
ritwikraha <rtzdzn@gmail.com>
2021-09-24 00:00:37 +0530

    new scripts

--- random-experiments/Manim-Mastery/Fun/DragonFractal.py
+++ random-experiments/Manim-Mastery/Fun/DragonFractal.py
@@ -5,65 +5,45 @@ from manim import *
 
 class Fractalify(MovingCameraScene):
     CONFIG = {
-        "iterations": 15,
-        "angle": 90 * DEGREES,
-        "border_proportion": 1.25,
-        "colors": [RED_A, RED_C, RED_E, BLUE_A,
-                   BLUE_C, BLUE_E, YELLOW_A, YELLOW_C,
-                   YELLOW_E, PURPLE_A, PURPLE_C, PURPLE_E]
+        "colors": [RED, ORANGE, GREEN_B, BLUE_D, BLUE]
     }
 
     def construct(self):
-        self.color = self.cycle(self.colors)
-        path = VGroup()
-        first_line = Line(ORIGIN, UP / 5, color=next(self.color))
-        path.add(first_line)
 
-        self.camera_frame.set_height(first_line.get_height() * self.border_proportion)
-        self.camera_frame.move_to(first_line)
-        self.play(Create(first_line))
-        self.add_foreground_mobject(path)
 
-        self.target_path = self.get_all_paths(path, self.iterations)
-        for i in range(self.iterations):
-            self.duplicate_path(path, i)
+        line = Line(DOWN, UP).scale(0.004)
+        self.offset = 1.4
+
+        self.dragon = VMobject()
+        self.dragon.set_stroke(width=20)
+        self.dragon.set_points(line.get_points())
+
+        self.frame = self.camera.frame
+        self.frame.set_height(self.dragon.get_height() * self.offset)
+        self.frame.move_to(self.dragon.get_center())
+
+        self.last_point = self.dragon.get_last_point()
+
+        self.play(Create(self.dragon.copy()))
+
+        for _ in range(10):
+            self.copy_and_rotate()
         self.wait()
 
-    def duplicate_path(self, path, i):
-        set_paths = self.target_path[:2 ** (i + 1)]
-        height = set_paths.get_height() * self.border_proportion
-        new_path = path.copy()
-        new_path.set_color(next(self.color))
-        self.add(new_path)
-        point = self.get_last_point(path)
-        self.play(
-            Rotating(
-                new_path,
-                radians=self.angle,
-                about_point=path[-1].points[point],
-                rate_func=linear
-            ),
-            self.camera_frame.move_to, set_paths,
-            self.camera_frame.set_height, height,
-            run_time=1, rate_func=smooth
+    def copy_and_rotate(self):
+        dragon = self.dragon.copy()
+        dragon.generate_target()
+        dragon.target.rotate(PI / 2, about_point=self.last_point)
+
+        self.bring_to_back(dragon)
+        self.dragon.set_points(
+            np.vstack((self.dragon.get_points(), dragon.target.get_points()))
         )
-        self.add_foreground_mobject(new_path)
-        post_path = reversed([*new_path])
-        path.add(*post_path)
 
-    def get_all_paths(self, path, iterations):
-        target_path = path.copy()
-        for _ in range(iterations):
-            new_path = target_path.copy()
-            point = self.get_last_point(new_path)
-            new_path.rotate(
-                self.angle,
-                about_point=target_path[-1].points[point],
-            )
-            post_path = reversed([*new_path])
-            target_path.add(*post_path)
+        self.play(
+            MoveToTarget(dragon, path_arc=PI / 2),
+            run_time=1.5,
+            rate_func=linear,
+        )
 
-        return target_path
-
-    def get_last_point(self, path):
-        return 0 if len(path) > 1 else -1
+        self.last_point = self.dragon.get_points()[len(self.dragon.get_points()) // 2]
